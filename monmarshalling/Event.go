6ec6a246dd2fbc1202acabdb2501c9e142a8320f
adgs <dummy>
2022-03-14 20:35:01 +0200

    HostName added to standard config struct + helper methods for Event

--- monmarshalling/Event.go
+++ monmarshalling/Event.go
@@ -1,10 +1,32 @@
 package monmarshalling
 
+import "time"
+
+const HeartBeatMessageTypeName = "heartbeat"
+const StatsBeatMessageTypeName = "stats"
+
 type MetaData struct {
-	Type          string
-	StatType      string
-	CollectUnixMs int64
-	PollRateMs    int64
+	MessageType          string
+	StatType             string
+	HostName             string
+	AgentTimestampUnixMs int64
+	PollRateMs           int
+}
+
+func NewMetaDataWithTs() *MetaData {
+	return &MetaData{AgentTimestampUnixMs: time.Now().UnixMilli()}
+}
+
+func NewHeartBeatMetaDataWithTs() *MetaData {
+	d := NewMetaDataWithTs()
+	d.MessageType = HeartBeatMessageTypeName
+	return d
+}
+
+func NewStatsMetaDataWithTs() *MetaData {
+	d := NewMetaDataWithTs()
+	d.MessageType = StatsBeatMessageTypeName
+	return d
 }
 
 type Stat struct {
--- monmarshalling/envconfig/StandardStatsServiceConfig.go
+++ monmarshalling/envconfig/StandardStatsServiceConfig.go
@@ -2,4 +2,5 @@ package envconfig
 
 type StatsConfig struct {
 	ServerUrl string `mapstructure:"stats_server_url"`
+	HostName  string `mapstructure:"host_name"`
 }
