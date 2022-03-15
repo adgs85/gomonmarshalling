00ead3038d72ab18ee36f5ff78894d462fe3dcc7
adgs <dummy>
2022-03-15 21:35:37 +0200

    added req timeout seconds

--- monmarshalling/envconfig/StandardStatsServiceConfig.go
+++ monmarshalling/envconfig/StandardStatsServiceConfig.go
@@ -1,6 +1,7 @@
 package envconfig
 
 type StatsConfig struct {
-	ServerUrl string `mapstructure:"stats_server_url"`
-	HostName  string `mapstructure:"host_name"`
+	ServerUrl         string `mapstructure:"stats_server_url"`
+	RequestTimeoutSec string `mapstructure:"request_timeout_sec"`
+	HostName          string `mapstructure:"host_name"`
 }
